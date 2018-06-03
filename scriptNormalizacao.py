ca8fc1d15408338d51e3adbc5d71292b48f58ef4
Luiz Fritsch <fritsch.luiz@gmail.com>
2018-06-03 18:30:52 -0300

    adição de comentários

--- scriptNormalizacao.py
+++ scriptNormalizacao.py
@@ -31,16 +31,27 @@ def read_csv_and_normalize(name):
 			reader = csv.reader(f)
 			i = 0
 			for row in reader:
-				
+				#Cada linha virava uma lista				
+				#transformo todos elementos dessa lista numa string unica
 				str1 = ''.join(row)
+
+				#crio uma lista com elementos que vao ate ;,  
 				list = str1.split(';')
+
+				#no csv temos dois tipos de elementos com apenas um indice
 				if len(list)==1:
+
+					#os que contem apenas o nome do bairro
 					if "na divis" not in list[0]:
 						local = list[0]
 						#for com qtd de vezes iterando em nmrPorSetor 
 						#localizacao.append(list[0])
+					
+					#E os que contem informacoes sobre a quantidade de terrenos naquele bairro, setor ou etc
 					else:
 						nmrPorSetor = int(list[0].split(' ', 1)[0])
+				
+				#para os elementos que tem 4, sabemos que eles contem informacoes sobre o terreno em si
 				if len(list)==4:
 					try:
 						descricao.append(list[0])
@@ -76,5 +87,5 @@ def main():
 	name = 'dadosImoveisPrefeituraAlegrete'
 	dict =  read_csv_and_normalize(name)
 	df = pd.DataFrame.from_dict(dict)
-	print df.head(1)
+
 main()
